2aa54e05babb7f94114ac9386deb2e79fae5d3fb
Amer Harb <amer@penni.io>
2022-09-06 20:44:02 +0200

    add variables to return

--- src/Step.ts
+++ src/Step.ts
@@ -3,11 +3,13 @@ import { Variable } from './Variable'
 
 type VarLabels = [(Variable | Label)?, ...Label[]]
 
-export class Step implements Match, Return {
+export class Step implements Root, Match, Return {
 	private matchParts: VarLabels = []
+	private returnParts: Variable[] = []
 
 	public match(variable: Variable): Match
 	public match(...Labels: Label[]): Match
+	public match(...varLabels: VarLabels): Match
 	public match(...varLabels: VarLabels): Match {
 		if (varLabels.length === 0) {
 			throw new Error('No variable or labels provided')
@@ -16,7 +18,8 @@ export class Step implements Match, Return {
 		return this
 	}
 
-	public return(): Return {
+	public return(...variables: Variable[]): Return {
+		this.returnParts.push(...variables)
 		return this
 	}
 
@@ -30,18 +33,30 @@ export class Step implements Match, Return {
 			? [first.name, ...this.matchParts.slice(1).map(label => `${label?.name}`)]
 			: ['', ...this.matchParts.map(label => `${label?.name}`)]
 
-		return `MATCH (${matchArray.join(':')}) RETURN n`
+		return `MATCH (${matchArray.join(':')}) RETURN ${this.returnParts.map(it => it.name).join(', ')}`
 	}
 
 	public cleanUp(): void {
 		this.matchParts.length = 0
+		this.returnParts.length = 0
 	}
 }
 
-export interface Match {
-	return(): Return
+export interface Match  extends BaseStep {
+	return(...variables: Variable[]): Return
 }
 
-export interface Return {
+export interface Return  extends BaseStep{
+	getCypher(): string
+}
+
+export interface Root extends BaseStep{
+	match(variable: Variable): Match
+	match(...Labels: Label[]): Match
+	match(...varLabels: VarLabels): Match
+}
+
+export interface BaseStep {
+	cleanUp(): void
 	getCypher(): string
 }
--- test/Step.test.ts
+++ test/Step.test.ts
@@ -7,33 +7,43 @@ const Animal = database.Labels.Animal
 const n = new sedk.Variable('n')
 
 describe('Step', () => {
-	const cypher = new sedk.Step()
+	const cypher = sedk.builder()
 	afterEach(() => {
 		cypher.cleanUp()
 	})
 	describe('simple Cypher', () => {
 		it('Produce: [MATCH (n) RETURN n]', () => {
-			const actual = cypher.match(n).return().getCypher()
+			const actual = cypher.match(n).return(n).getCypher()
 
 			expect(actual).toBe('MATCH (n) RETURN n')
 		})
+		it('Produce: [MATCH (n) RETURN n, n]', () => {
+			const actual = cypher.match(n).return(n, n).getCypher()
+
+			expect(actual).toBe('MATCH (n) RETURN n, n')
+		})
+		it('Produce: [MATCH (n) RETURN n, n, n]', () => {
+			const actual = cypher.match(n).return(n, n, n).getCypher()
+
+			expect(actual).toBe('MATCH (n) RETURN n, n, n')
+		})
 		it('Produce: [MATCH (:Person) RETURN n]', () => {
-			const actual = cypher.match(database.Labels.Person).return().getCypher()
+			const actual = cypher.match(database.Labels.Person).return(n).getCypher()
 
 			expect(actual).toBe('MATCH (:Person) RETURN n')
 		})
 		it('Produce: [MATCH (:Person:Animal) RETURN n]', () => {
-			const actual = cypher.match(Person, Animal).return().getCypher()
+			const actual = cypher.match(Person, Animal).return(n).getCypher()
 
 			expect(actual).toBe('MATCH (:Person:Animal) RETURN n')
 		})
 		it('Produce: [MATCH (n:Person) RETURN n]', () => {
-			const actual = cypher.match(n, Person).return().getCypher()
+			const actual = cypher.match(n, Person).return(n).getCypher()
 
 			expect(actual).toBe('MATCH (n:Person) RETURN n')
 		})
 		it('Produce: [MATCH (n:Person:Animal) RETURN n]', () => {
-			const actual = cypher.match(n, Person, Animal).return().getCypher()
+			const actual = cypher.match(n, Person, Animal).return(n).getCypher()
 
 			expect(actual).toBe('MATCH (n:Person:Animal) RETURN n')
 		})

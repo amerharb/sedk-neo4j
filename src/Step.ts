d3a1d5a62f933c3c20b484bf2a4cd548916d2c4d
Amer Harb <amer@penni.io>
2022-09-06 20:12:55 +0200

    rename matchLabels to matchParts

--- src/Step.ts
+++ src/Step.ts
@@ -4,7 +4,7 @@ import { Variable } from './Variable'
 type VarLabels = [(Variable | Label)?, ...Label[]]
 
 export class Step implements Match, Return {
-	private matchLabels: VarLabels = []
+	private matchParts: VarLabels = []
 
 	public match(variable: Variable): Match
 	public match(...Labels: Label[]): Match
@@ -12,7 +12,7 @@ export class Step implements Match, Return {
 		if (varLabels.length === 0) {
 			throw new Error('No variable or labels provided')
 		}
-		this.matchLabels.push(...varLabels)
+		this.matchParts.push(...varLabels)
 		return this
 	}
 
@@ -21,20 +21,20 @@ export class Step implements Match, Return {
 	}
 
 	public getCypher(): string {
-		const first = this.matchLabels[0]
+		const first = this.matchParts[0]
 		if (first === undefined) {
 			throw new Error('No variable or labels provided')
 		}
 
 		const matchArray = (first instanceof Variable)
-			? [first.name, ...this.matchLabels.slice(1).map(label => `${label?.name}`)]
-			: ['', ...this.matchLabels.map(label => `${label?.name}`)]
+			? [first.name, ...this.matchParts.slice(1).map(label => `${label?.name}`)]
+			: ['', ...this.matchParts.map(label => `${label?.name}`)]
 
 		return `MATCH (${matchArray.join(':')}) RETURN n`
 	}
 
 	public cleanUp(): void {
-		this.matchLabels.length = 0
+		this.matchParts.length = 0
 	}
 }
 

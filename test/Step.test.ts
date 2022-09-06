ef7c4bb1c132d60e576ab6e5047f2fb09335bf87
Amer Harb <amer@penni.io>
2022-09-06 10:49:10 +0200

    test cover all cases 100% ;)

--- test/Step.test.ts
+++ test/Step.test.ts
@@ -6,34 +6,49 @@ const Person = database.Labels.Person
 const Animal = database.Labels.Animal
 const n = new sedk.Variable('n')
 
-describe('simple Cypher', () => {
+describe('Step', () => {
 	const cypher = new sedk.Step()
 	afterEach(() => {
 		cypher.cleanUp()
 	})
-	it('Produce: [MATCH (n) RETURN n]', () => {
-		const actual = cypher.match(n).return().getCypher()
+	describe('simple Cypher', () => {
+		it('Produce: [MATCH (n) RETURN n]', () => {
+			const actual = cypher.match(n).return().getCypher()
 
-		expect(actual).toBe('MATCH (n) RETURN n')
+			expect(actual).toBe('MATCH (n) RETURN n')
+		})
+		it('Produce: [MATCH (:Person) RETURN n]', () => {
+			const actual = cypher.match(database.Labels.Person).return().getCypher()
+
+			expect(actual).toBe('MATCH (:Person) RETURN n')
+		})
+		it('Produce: [MATCH (:Person:Animal) RETURN n]', () => {
+			const actual = cypher.match(Person, Animal).return().getCypher()
+
+			expect(actual).toBe('MATCH (:Person:Animal) RETURN n')
+		})
+		it('Produce: [MATCH (n:Person) RETURN n]', () => {
+			const actual = cypher.match(n, Person).return().getCypher()
+
+			expect(actual).toBe('MATCH (n:Person) RETURN n')
+		})
+		it('Produce: [MATCH (n:Person:Animal) RETURN n]', () => {
+			const actual = cypher.match(n, Person, Animal).return().getCypher()
+
+			expect(actual).toBe('MATCH (n:Person:Animal) RETURN n')
+		})
 	})
-	it('Produce: [MATCH (:Person) RETURN n]', () => {
-		const actual = cypher.match(database.Labels.Person).return().getCypher()
 
-		expect(actual).toBe('MATCH (:Person) RETURN n')
-	})
-	it('Produce: [MATCH (:Person:Animal) RETURN n]', () => {
-		const actual = cypher.match(Person, Animal).return().getCypher()
+	describe('Throwing Errors', () => {
+		it('Throws: ', () => {
+			const actual = () => cypher.match()
 
-		expect(actual).toBe('MATCH (:Person:Animal) RETURN n')
-	})
-	it('Produce: [MATCH (n:Person) RETURN n]', () => {
-		const actual = cypher.match(n, Person).return().getCypher()
+			expect(actual).toThrow('No variable or labels provided')
+		})
+		it('Throws: ', () => {
+			const actual = () => cypher.getCypher()
 
-		expect(actual).toBe('MATCH (n:Person) RETURN n')
-	})
-	it('Produce: [MATCH (n:Person:Animal) RETURN n]', () => {
-		const actual = cypher.match(n, Person, Animal).return().getCypher()
-
-		expect(actual).toBe('MATCH (n:Person:Animal) RETURN n')
+			expect(actual).toThrow('No variable or labels provided')
+		})
 	})
 })
